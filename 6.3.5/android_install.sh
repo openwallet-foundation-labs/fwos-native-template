d22d75e0f607944f6760aff27790faf58d393a69
skullquake <ockert8080@gmail.com>
2023-03-31 12:58:30 +0200

    .

--- 6.3.5/android_install.sh
+++ 6.3.5/android_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /mnt/c/usr/bin/say.exe installing android application
 find ./prj|grep "app-dev-debug.apk"|while read APK;do
-	echo installing archive $APK
+	echo installing $APK
 	/mnt/c/usr/bin/adb.exe install -g "$APK"
 done
