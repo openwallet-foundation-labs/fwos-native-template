cb8f4aae4c18c7ef2fcc4ba7176913402bc4b31e
skullquake <ockert8080@gmail.com>
2023-06-29 23:38:20 +0200

    .
    Signed-off-by: Jorge Flores <jorge@entidad.io>

---
+++ 6.3.5_0.3.4/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SAY=sam
+SAY=say
+$SAY installing
+cd ./prj
+#npm install --legacy-peer-deps
+# 0.3.0 
+#npm install @aries-framework/core
+#npm install @aries-framework/react-native
+#npm install react-native-fs
+#npm install react-native-get-random-values
+#npm install indy-sdk-react-native
+# 0.4.0 
+npm install @aries-framework/react-native@^0.4.0
+npm install @aries-framework/core@^0.4.0
+npm install @aries-framework/indy-sdk@^0.4.0
+npm install @aries-framework/anoncreds@^0.4.0
+npm install @aries-framework/anoncreds-rs@^0.4.0
+npm install @hyperledger/anoncreds-react-native@^0.1.0
+npm install indy-sdk-react-native@^0.3.1
+npm install --save-dev @types/indy-sdk-react-native@npm:@types/indy-sdk
+$SAY installation complete
---
+++ 6.3.5_0.4.0/prj/android/app/src/appstore/java/com/mendix/nativetemplate/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mendix.nativetemplate;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.mendix.mendixnative.activity.MendixReactActivity;
+import com.mendix.mendixnative.config.AppUrl;
+import com.mendix.mendixnative.react.MendixApp;
+import com.mendix.mendixnative.react.MxConfiguration;
+
+public class MainActivity extends MendixReactActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        this.getLifecycle().addObserver(new MendixActivityObserver(this));
+        Boolean hasDeveloperSupport = ((MainApplication) getApplication()).getUseDeveloperSupport();
+        mendixApp = new MendixApp(AppUrl.getUrlFromResource(this), MxConfiguration.WarningsFilter.none, hasDeveloperSupport, false);
+        super.onCreate(savedInstanceState);
+    }
+}
