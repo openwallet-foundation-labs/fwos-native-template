be7fb322a4bd4deca4dffe48cfda4f7d8106074d
Jorge Flores <jorge@entidad.io>
2023-07-03 08:42:37 -0700

    deleted node_modules from commit and added .gitignore file under prj
    Signed-off-by: Jorge Flores <jorge@entidad.io>

--- 7.0.1_0.3.3/prj/node_modules/code-push/script/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var AccountManager = require("./management-sdk");
-module.exports = AccountManager;
--- 7.0.1_0.3.3/prj/node_modules/detox/android/detox/src/testFull/java/com/wix/detox/UTHelpers.kt
+++
@@ -1,8 +0,0 @@
-package com.wix.detox
-
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.TimeUnit
-
-object UTHelpers {
-    fun yieldToOtherThreads(executor: ExecutorService) = executor.awaitTermination(100L, TimeUnit.MILLISECONDS)
-}
