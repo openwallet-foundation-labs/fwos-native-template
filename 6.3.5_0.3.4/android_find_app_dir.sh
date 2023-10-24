d79f718d3a23545055fe24c4201da8981c545b22
Ockert van Schalkwyk <ockert.vanschalkwyk@entidad.io>
2023-10-24 13:12:39 +0200

    remove 6.3.5_0.3.4
    Signed-off-by: Ockert van Schalkwyk <ockert.vanschalkwyk@entidad.io>

--- 6.3.5_0.3.4/android_find_app_dir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find ./prj|grep "app-dev-debug.apk"|while read APK;do
-	PACKAGENAME=$(aapt dump badging "$APK"|grep package | awk '{print $2}' | sed s/name=//g | sed s/\'//g)
-	echo $PACKAGENAME
-	/mnt/c/usr/bin/adb.exe shell 'file $(find $(dirname $(pm path '$PACKAGENAME'|cut -f2- -d'"'"':'"'"')))'
-done
