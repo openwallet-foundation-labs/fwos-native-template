59111826d16444f2580dd46005da3769b1de7c9b
Jorge Flores <jorge@entidad.io>
2023-11-01 11:04:40 -0700

    Updated dependencies to AFJ 0.4.1 (#7)
    * Fix AriesJS package.json
    
    Signed-off-by: Ockert van Schalkwyk <ockert.vanschalkwyk@entidad.io>
    
    * updated bundleID
    
    Signed-off-by: Jorge Flores <jorge@entidad.io>
    
    ---------
    
    Signed-off-by: Ockert van Schalkwyk <ockert.vanschalkwyk@entidad.io>
    Signed-off-by: Jorge Flores <jorge@entidad.io>
    Co-authored-by: Ockert van Schalkwyk <ockert.vanschalkwyk@entidad.io>

--- source/install.sh
+++ source/install.sh
@@ -4,16 +4,17 @@ SAY=say
 $SAY installing
 cd ./prj
 npm install --legacy-peer-deps && \
-npm install @aries-framework/react-native@^0.4.1 && \
-npm install @aries-framework/core@^0.4.1 && \
-npm install @aries-framework/anoncreds@^0.4.1 && \
-npm install @aries-framework/anoncreds-rs@^0.4.1 && \
-npm install @aries-framework/cheqd@^0.4.1 && \
-npm install @hyperledger/anoncreds-react-native@^0.1.0 && \
+npm install @aries-framework/react-native@0.4.1 && \
+npm install @aries-framework/core@0.4.1 && \
+npm install @aries-framework/anoncreds@0.4.1 && \
+npm install @aries-framework/anoncreds-rs@0.4.1 && \
+npm install @aries-framework/cheqd@0.4.1 && \
+npm install @aries-framework/askar@0.4.1 && \
+npm install @hyperledger/anoncreds-react-native@0.1.0 && \
 npm install @hyperledger/aries-askar-react-native@0.1.0 && \
 npm install @aries-framework/indy-vdr@0.4.1 && \
 npm install @hyperledger/indy-vdr-shared@0.1.0 && \
-npm install @hyperledger/indy-vdr-react-native@^0.1.0 && \
+npm install @hyperledger/indy-vdr-react-native@0.1.0 && \
 npm install react-native-sensitive-info@next && \
 npm install react-native-fs && \
 npm install react-native-get-random-values && \
